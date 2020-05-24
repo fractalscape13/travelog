8ee5a404e3b28b21d3f1fede773c83962aff2285
Joseph Wangemann <fractalscape13@gmail.com>
2020-05-23 23:18:59 -0700

    attempt to add colorthief

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './../App.css';
+import tulum from '../assets/tulum.png'
+import ColorThief from "colorthief";
 
 function App() {
 
-  const imgSrc = '../assets/tulum.png';
+    const img = new Image();
+    img.source = {tulum};
 
   return (
     <div>
-      <img href={imgSrc} />
+      <img src={tulum} alt="tulum" onLoad={() => {
+              const colorThief = new ColorThief();
+              const result = colorThief.getColor(img.source, 25);
+              console.log("RESULT::", result)
+            }}/>
       <h1>Travelog</h1>
       <p>Check this shit out</p>
     </div>
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducers/reducer';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <Provider store={store}>
+  // <Provider store={store}>
+  //   <App />
+  // </Provider>,
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
