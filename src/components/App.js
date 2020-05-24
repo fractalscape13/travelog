d7b92e72dc9cf064f553b3c1a651d6a8ba4a3269
Joseph Wangemann <fractalscape13@gmail.com>
2020-05-24 00:34:36 -0700

    attempt to add colorthief

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,27 @@ function App() {
 
     const img = new Image();
     img.source = {tulum};
+    let colors = [];
+
+    thiefColor(img, index) {
+      const result = ColorThief.getColorAsync(img).then(data => {
+        const rgb = ColorThief.convertColorRgb(data)
+        colors[index] = rgb
+      })
+      console.log(result)
+    }
 
   return (
     <div>
-      <img src={tulum} alt="tulum" onLoad={() => {
+      {/* <img src={tulum} alt="tulum" onLoad={() => {
               const colorThief = new ColorThief();
               const result = colorThief.getColor(img.source, 25);
               console.log("RESULT::", result)
-            }}/>
+            }}/> */}
+      <img src={tulum} alt="tulum" />
       <h1>Travelog</h1>
       <p>Check this shit out</p>
+      <div className="stripe"></div>
     </div>
   );
 }
